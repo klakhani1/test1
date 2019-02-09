4ce0114736208c0e1d6e26fea2c712bac25638d3
Klakhani1 <lakhanikazim12@gmail.com>
2019-02-09 12:51:40 -0600

    added final line to project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+ffrom flask import Flask, render_template, request
 import my_yelp
 
 app = Flask(__name__)
--- my_yelp.py
+++ my_yelp.py
@@ -1,38 +1,28 @@
 
-   businesses_dict = json.loads(businesses_object.text)
+def search_businesses(search_term, search_location):
 
+    api_key = "_JnBPi2QeXRYaSqoYutrlVBoqVQp_UYELb6MUSRKWBWmPCFnesM9S1leyTV6FYqiqoGi3k_EPZXctlVWDH4cuEoriv_smBiwUk6A8-F8t38WT-59HSHYb5wVBNhVXHYx"
 
+    url = "https://api.yelp.com/v3/businesses/search"
 
+    my_headers = {
+        "Authorization": "Bearer %s" % api_key
+    }
 
+    my_params = {
+        "term": search_term,
+        "location": search_location,
+        "limit": 3,
+    }
 
+    businesses_object = requests.get(url, headers=my_headers, params=my_params)
 
+    businesses_dict = json.loads(businesses_object.text)
 
     businesses_list = businesses_dict['businesses']
 
-
-
-    #
-
-
-
-
-
-
-
     print(businesses_dict)
-
-
-
     list_of_businesses = []
-
-
-
     for each in businesses_list:
-
-
-
         list_of_businesses.append(each)
-
-
-
     return list_of_businesses
